33d143bf82654f2d165389091e391fedec7c162d
Sakshi kalyan jadhav <sakshikalyanjadhav@Sakshis-MacBook-Air.local>
2024-07-19 15:03:05 +0530

    add render url

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://linkedin-gemini-gs-server.onrender.com",
 });
 
 export const fetchSheetDataApi = () => API.get("/api/v1/sheets");
